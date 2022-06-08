7c6b078c262a859d41de3c46e0b3a9dcdbe4062a
ahmed <ahmedfahim119@gmail.com>
2022-06-08 21:45:23 +0200

    a new modification in C++ file added -the loop - which is printing 2 values happend

--- print 2 numbers.cpp
+++ print 2 numbers.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 
-float arithmaticmean(int x_1 , int x_2) {
+float arithmaticmean(float x_1 , float x_2) {
     float x = (x_2 + x_1) / 2 ;
 return  x ; 
 }
 
-float geometricmean(int x_1 , int x_2)
+float geometricmean(float x_1 , float x_2)
 {
     float y = x_2 * x_1 ;
     float GeoMean=sqrt(y);
@@ -18,16 +18,55 @@ float geometricmean(int x_1 , int x_2)
 
 int main()
 {
-    int x_1 , x_2 ;
+    float x_1 , x_2 ;
     cout<<"please Enter the two values\n";
-    cin >> x_1 ;
-    cin >> x_2 ;
-    cout << "THE FIRST VALUE IS "<< x_1 <<endl;
-    cout << "THE SECOND VALUE IS "<<x_2 <<endl;
-    cout << "Arithmaticmean is : "<< arithmaticmean(x_1,x_2)<<endl;
-    cout << "THE geometricmean is : "<<geometricmean(x_2,x_1)<<endl;
-    return 0;
+        cin >> x_1 ;
+        cin >> x_2 ;
+    while (x_1 && x_2 > 0)
+    {
+        cout << "THE FIRST VALUE IS "<< x_1 <<endl;
+        cout << "THE SECOND VALUE IS "<<x_2 <<endl;
+        cout << "Arithmaticmean is : "<< arithmaticmean(x_1,x_2)<<endl;
+        cout << "THE geometricmean is : "<<geometricmean(x_2,x_1)<<endl;
+        cout<<"please Enter the two values\n";
+        cin >> x_1 ;
+        cin >> x_2 ;
+    }
+        return 0;
 }
+
+
+
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+
+
+// float arithmaticmean(int x_1 , int x_2) {
+//     float x = (x_2 + x_1) / 2 ;
+// return  x ; 
+// }
+
+// float geometricmean(int x_1 , int x_2)
+// {
+//     float y = x_2 * x_1 ;
+//     float GeoMean=sqrt(y);
+//     return GeoMean;
+// }
+
+
+// int main()
+// {
+//     int x_1 , x_2 ;
+//     cout<<"please Enter the two values\n";
+//     cin >> x_1 ;
+//     cin >> x_2 ;
+//     cout << "THE FIRST VALUE IS "<< x_1 <<endl;
+//     cout << "THE SECOND VALUE IS "<<x_2 <<endl;
+//     cout << "Arithmaticmean is : "<< arithmaticmean(x_1,x_2)<<endl;
+//     cout << "THE geometricmean is : "<<geometricmean(x_2,x_1)<<endl;
+//     return 0;
+// }
 
 
 
