b3e581ad2a433bb37caf4c800afeb2b49f783633
ahmed <ahmedfahim119@gmail.com>
2022-06-08 20:47:59 +0200

    a new C++ file add which is printing 2 values

---
+++ print 2 numbers.cpp
@@ -0,0 +1,16 @@
+
+include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x_1 , x_2 ;
+    cout<<"please Enter the two values\n";
+    cin >> x_1 ;
+    cin >> x_2 ;
+    cout << "THE FIRST VALUE IS "<< x_1 <<endl;
+    cout << "THE SECOND VALUE IS "<<x_2 <<endl;
+
+    return 0;
+}
