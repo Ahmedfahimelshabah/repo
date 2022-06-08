d584e8c7c7ec8bb3f5b8153b6eb793c24670b680
ahmed <ahmedfahim119@gmail.com>
2022-06-08 21:21:02 +0200

    a new modification in C++ file added -the GEomatric mean- which is printing 2 values happend

--- print 2 numbers.cpp
+++ print 2 numbers.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+
 float arithmaticmean(int x_1 , int x_2) {
-    float x ;
-    x = (x_2 + x_1) / 2 ;
+    float x = (x_2 + x_1) / 2 ;
 return  x ; 
+}
+
+float geometricmean(int x_1 , int x_2)
+{
+    float y = x_2 * x_1 ;
+    float GeoMean=sqrt(y);
+    return GeoMean;
 }
 
 
@@ -18,13 +25,39 @@ int main()
     cout << "THE FIRST VALUE IS "<< x_1 <<endl;
     cout << "THE SECOND VALUE IS "<<x_2 <<endl;
     cout << "Arithmaticmean is : "<< arithmaticmean(x_1,x_2)<<endl;
-
+    cout << "THE geometricmean is : "<<geometricmean(x_2,x_1)<<endl;
     return 0;
 }
 
 
 
-// include <iostream>
+// #include <iostream>
+
+// using namespace std;
+
+// float arithmaticmean(int x_1 , int x_2) {
+//     float x ;
+//     x = (x_2 + x_1) / 2 ;
+// return  x ; 
+// }
+
+
+// int main()
+// {
+//     int x_1 , x_2 ;
+//     cout<<"please Enter the two values\n";
+//     cin >> x_1 ;
+//     cin >> x_2 ;
+//     cout << "THE FIRST VALUE IS "<< x_1 <<endl;
+//     cout << "THE SECOND VALUE IS "<<x_2 <<endl;
+//     cout << "Arithmaticmean is : "<< arithmaticmean(x_1,x_2)<<endl;
+
+//     return 0;
+// }
+
+
+
+// #include <iostream>
 
 // using namespace std;
 
