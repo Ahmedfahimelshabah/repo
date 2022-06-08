85122eb45db6187ab68ff972950f3a533b518695
ahmed <ahmedfahim119@gmail.com>
2022-06-08 21:03:55 +0200

    a new modification in C++ file add which is printing 2 values happend

--- print 2 numbers.cpp
+++ print 2 numbers.cpp
@@ -1,7 +1,13 @@
-
-include <iostream>
+#include <iostream>
 
 using namespace std;
+
+float arithmaticmean(int x_1 , int x_2) {
+    float x ;
+    x = (x_2 + x_1) / 2 ;
+return  x ; 
+}
+
 
 int main()
 {
@@ -11,6 +17,25 @@ int main()
     cin >> x_2 ;
     cout << "THE FIRST VALUE IS "<< x_1 <<endl;
     cout << "THE SECOND VALUE IS "<<x_2 <<endl;
+    cout << "Arithmaticmean is : "<< arithmaticmean(x_1,x_2)<<endl;
 
     return 0;
 }
+
+
+
+// include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int x_1 , x_2 ;
+//     cout<<"please Enter the two values\n";
+//     cin >> x_1 ;
+//     cin >> x_2 ;
+//     cout << "THE FIRST VALUE IS "<< x_1 <<endl;
+//     cout << "THE SECOND VALUE IS "<<x_2 <<endl;
+
+//     return 0;
+// }
